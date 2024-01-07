7c5c6bd6afcc9304fd4623b662f3de87305d4213
Wolfgang Weck <wolfgang.weck@surface7>
2024-01-07 14:49:02 +0100

    revised tests - no more usage of `new Integer()` - additional stability test for not completely sorted date

--- src/test/java/ch/fhnw/algd1/mergesort/MergesortTest.java
+++ src/test/java/ch/fhnw/algd1/mergesort/MergesortTest.java
@@ -1,6 +1,7 @@
 package ch.fhnw.algd1.mergesort;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 
@@ -8,6 +9,23 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MergesortTest {
+	private static final class MyInteger implements Comparable<MyInteger> {
+		private final int val;
+
+		private MyInteger(int val) {
+			this.val = val;
+		}
+
+		@Override
+		public int compareTo(MyInteger other) {
+			return val - other.val;
+		}
+
+		@Override
+		public boolean equals(Object other) {
+			return val == ((MyInteger)other).val;
+		}
+	}
 
 	private Mergesort sort;
 
@@ -22,10 +40,10 @@ public class MergesortTest {
 		sortAndCheck(transform(numbers));
 	}
 
-	private Integer[] transform(int[] numbers) {
-		Integer[] arr = new Integer[numbers.length];
+	private MyInteger[] transform(int[] numbers) {
+		MyInteger[] arr = new MyInteger[numbers.length];
 		for (int i = 0; i < numbers.length; i++) {
-			arr[i] = new Integer(numbers[i]);
+			arr[i] = new MyInteger(numbers[i]);
 		}
 		return arr;
 	}
@@ -74,17 +92,25 @@ public class MergesortTest {
 
 	@Test
 	public void testStability() {
-		String one = new String("Z"), two = new String("Z"), three = new String(
-				"Z"), four = new String("Z"), five = new String("Z"), six = new String(
-				"Z"), seven = new String("Z"), eight = new String("Z"), nine = new String(
-				"Z"), ten = new String("Z");
-		String[] original = new String[] { one, two, three, four, five, six,
-				seven, eight, nine, ten };
-
-		String[] strings = new String[] { one, two, three, four, five, six,
-				seven, eight, nine, ten };
+		String one = new String("Z"), two = new String("Z"), three = new String("Z"), four = new String("Z"),
+				five = new String("Z"), six = new String("Z"), seven = new String("Z"), eight = new String("Z"),
+				nine = new String("Z"), ten = new String("Z");
+		String[] original = new String[] { one, two, three, four, five, six, seven, eight, nine, ten };
+		String[] strings = new String[] { one, two, three, four, five, six, seven, eight, nine, ten };
 		sortAndCheck(strings);
+		for (int i = 0; i < original.length; i++) {
+			assertTrue(strings[i] == original[i]);
+		}
+	}
 
+	@Test
+	public void testStability2() {
+		String one = new String("A"), two = new String("A"), three = new String("A"), four = new String("B"),
+				five = new String("B"), six = new String("B"), seven = new String("Z"), eight = new String("Z"),
+				nine = new String("Z"), ten = new String("Z");
+		String[] original = new String[] { one, two, three, four, five, six, seven, eight, nine, ten };
+		String[] strings = new String[] { one, four, two, three, seven, eight, five, six, nine, ten };
+		sortAndCheck(strings);
 		for (int i = 0; i < original.length; i++) {
 			assertTrue(strings[i] == original[i]);
 		}
@@ -92,25 +118,19 @@ public class MergesortTest {
 
 	private <T extends Comparable<T>> void sortAndCheck(T[] numbers) {
 		T[] original = Arrays.copyOf(numbers, numbers.length);
-		sort.mergesort(numbers);		
+		sort.mergesort(numbers);
 		assertEquals("Number of elements differs from original", original.length, numbers.length);
 		assertTrue("Elements are not in ascending order", isAscending(numbers));
 		Arrays.sort(original);
 		assertTrue("Sorted and original elements are different", Arrays.equals(original, numbers));
-
-		
-		
 	}
 
 	private <T extends Comparable<T>> boolean isAscending(T[] a) {
-		if (a.length == 0)
-			return true;
-
+		if (a.length == 0) return true;
 		int i = 1;
 		while (i < a.length && a[i - 1].compareTo(a[i]) <= 0) {
 			i++;
 		}
 		return i == a.length;
 	}
-
 }
